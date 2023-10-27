a2923691dca1387042fb4bc990a120c334345128
2200031215 <147698689+2200031215@users.noreply.github.com>
2023-10-27 21:59:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from './components/NavigationBar';
+import UserForm from './components/UserForm';
+
+function App() {
+  return (
+    <div>
+      <NavigationBar /> 
+      <UserForm /> 
+    </div>
+  );
+}
+
+export default App;
